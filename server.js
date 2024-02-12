b81f1720fa302ebc5500277e8a13ca816f5509fb
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-02-12 22:29:40 +0000

    Port set to 8001

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const newsRoutes = require('./newsRoutes');
 const app = express();
-const PORT = 8000;
+const PORT = 8001;
 
 app.use('/', newsRoutes);
 app.use(express.static('public'));
