7dc2831afe7f3c51c8e29941b3eef3251644dbe1
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-02-12 23:30:24 +0000

    app converted into serverless-http

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const serverless = require('serverless-http');
 const newsRoutes = require('./newsRoutes');
 const app = express();
-const PORT = 8001;
+// const PORT = 8001;
 
 app.use('/', newsRoutes);
 app.use(express.static('public'));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports.handler = serverless(app);
