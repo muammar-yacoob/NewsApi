eaac1fe5f03b26712394f7c2393b1dcc7957ddf6
Muammar Yacoob <Muammar.Yacoob@gmail.com>
2024-02-14 19:06:01 +0000

    netlify setup on http://localhost:8888/api/news/?keyword=war&name=bbc

---
+++ functions/api.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const serverless = require('serverless-http');
+const newsRoutes = require('../src/newsRoutes');
+const app = express();
+const PORT = 8001;
+
+app.use('/', newsRoutes);
+app.use(express.static('public'));
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports.handler = serverless(app);
